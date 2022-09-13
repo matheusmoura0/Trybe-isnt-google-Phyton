7a0f8ebe27dc8e20929c97054f98f328b8481ca5
Matheus <matheusmoura2110@gmail.com>
2022-09-13 15:59:04 -0300

    feat: search function

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -12,4 +12,6 @@ class Queue:
         return self._data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0:
+            raise IndexError
+        return self._data[index]
