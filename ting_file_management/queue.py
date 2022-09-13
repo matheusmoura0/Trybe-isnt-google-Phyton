46f5cbb886093a42952623a2f2e6e14e52d1077f
Matheus <matheusmoura2110@gmail.com>
2022-09-13 15:48:38 -0300

    starting project

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,9 +1,9 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+       return len(self._data)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
