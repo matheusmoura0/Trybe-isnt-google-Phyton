3e004ad0ac53f6300f792b9d65969550b87fed05
Matheus <matheusmoura2110@gmail.com>
2022-09-14 11:12:08 -0300

    feat: implements txt_importer function

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        print('Formato inválido', file=sys.stderr)
+    
+
+    try:
+        file = []
+        with open(path_file, 'r') as f:
+            file = f.read().split("\n")
+            return file
+    except FileNotFoundError:
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
